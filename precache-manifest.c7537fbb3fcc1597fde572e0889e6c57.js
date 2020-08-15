cc638e46914f56ea55309b9b1b875a8ae57b7fc4
arjitj87 <arjit.jindal@unacademy.com>
2020-08-16 01:38:44 +0530

    Updates

---
+++ precache-manifest.c7537fbb3fcc1597fde572e0889e6c57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac36c23e91927d0b89fd6fb3d484dba",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "fd379dcc89591ac301cc",
+    "url": "/react-project/static/css/main.5eb42a7a.chunk.css"
+  },
+  {
+    "revision": "92e83192271f7c20698e",
+    "url": "/react-project/static/js/2.be90933c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.be90933c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd379dcc89591ac301cc",
+    "url": "/react-project/static/js/main.99d80e50.chunk.js"
+  },
+  {
+    "revision": "ef0ba607d25339a84d10",
+    "url": "/react-project/static/js/runtime-main.ec1ffa47.js"
+  },
+  {
+    "revision": "716ad49c461ab788525629843f0aa2d9",
+    "url": "/react-project/static/media/image1.716ad49c.jpg"
+  }
+]);
